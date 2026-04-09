f35108a858cd46c3598531d44cbd2a151d12dddf
ItoWindsor <itowindsor@gmail.com>
2026-04-09 12:00:00 +0200

    ADDED TESTS

--- tests/test_card.py
+++ tests/test_card.py
@@ -4,6 +4,9 @@ from src.card import CardSuit, CardRank
 from src.card import Card
 
 def test_card_comparison():
+    """
+    Check if the basic card comparison are well defined
+    """
     king = Card(CardRank.KING, CardSuit.SPADES)
     queen = Card(CardRank.QUEEN, CardSuit.HEARTS)
     jack = Card(CardRank.JACK, CardSuit.CLUBS)
